29d2eaa8fae445547351a6b4bced0939925f5b7b
Esteban Borai <estebanborai@gmail.com>
2023-04-21 14:07:04 -0400

    revert: changes on `sink` after move due to conflict

--- crates/http-sink/src/sink.rs
+++ crates/http-sink/src/sink.rs
@@ -1,8 +1,9 @@
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
+use reqwest::{Client, RequestBuilder};
+
 use fluvio::Offset;
 use fluvio_connector_common::{tracing::trace, LocalBoxSink, Sink};
-use reqwest::{Client, RequestBuilder};
 
 use crate::HttpConfig;
 
@@ -21,7 +22,7 @@ impl HttpSink {
 
         let headers = config.headers.iter().flat_map(|h| h.split_once(':'));
         for (key, value) in headers {
-            request = request.header(key, value);
+            request = request.header(key, value.trim());
         }
 
         Ok(Self { request })
@@ -32,7 +33,6 @@ impl HttpSink {
 impl Sink<String> for HttpSink {
     async fn connect(self, _offset: Option<Offset>) -> Result<LocalBoxSink<String>> {
         let request = self.request;
-
         let unfold = futures::sink::unfold(
             request,
             |mut request: RequestBuilder, record: String| async move {
@@ -48,6 +48,32 @@ impl Sink<String> for HttpSink {
                 Ok::<_, anyhow::Error>(request)
             },
         );
+
         Ok(Box::pin(unfold))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn builds_http_client_from_config() {
+        let config = HttpConfig {
+            endpoint: "http://localhost:8080".parse().unwrap(),
+            user_agent: "fluvio/http-sink 0.1.0".into(),
+            method: "POST".into(),
+            headers: vec!["Content-Type: text/html".into()],
+        };
+        let sink = HttpSink::new(&config).unwrap();
+        let req = sink.request.build().unwrap();
+
+        assert_eq!(req.headers().get("Content-Type").unwrap(), "text/html");
+        assert_eq!(
+            req.headers().get("User-Agent").unwrap(),
+            "fluvio/http-sink 0.1.0"
+        );
+        assert_eq!(req.method().to_string(), "POST");
+        assert_eq!(req.url().to_string(), "http://localhost:8080/");
     }
 }
