f6e6e6bd0f25df88c7c6a33beed3957c7f6e62d7
Esteban Borai <estebanborai@gmail.com>
2023-06-07 18:09:03 -0400

    feat: use crate version for logs

--- crates/http-sink/src/main.rs
+++ crates/http-sink/src/main.rs
@@ -8,6 +8,8 @@ use sink::HttpSink;
 
 use fluvio_connector_common::{connector, consumer::ConsumerStream, tracing, Sink};
 
+const SIGNATURES: &str = concat!("InfinyOn HTTP Sink Connector ", env!("CARGO_PKG_VERSION"));
+
 #[connector(sink)]
 async fn start(config: HttpConfig, mut stream: impl ConsumerStream) -> Result<()> {
     tracing::debug!(?config);
@@ -15,7 +17,7 @@ async fn start(config: HttpConfig, mut stream: impl ConsumerStream) -> Result<()
     let sink = HttpSink::new(&config)?;
     let mut sink = sink.connect(None).await?;
 
-    tracing::info!("Starting HTTP Sink Connector");
+    tracing::info!("Starting {SIGNATURES}");
     while let Some(item) = stream.next().await {
         tracing::debug!("Received record in consumer");
         let str = String::from_utf8(item?.as_ref().to_vec())?;
